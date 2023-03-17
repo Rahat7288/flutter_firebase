82a6a6387fa29df12ffcdfe73f16147d99cd4f27
Rahat Islam Akash <rahatislamakash@gmail.com>
2023-03-17 15:49:47 +0600

    adding the custom error for the Signin page if the state is in the srror state

--- lib/pages/sign_page.dart
+++ lib/pages/sign_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter_firebase/pages/signup_page.dart';
 import 'package:validators/validators.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../utils/error.dialog.dart';
+
 class SignPage extends StatefulWidget {
   static const String routeName = '/signin';
   const SignPage({super.key});
@@ -33,104 +35,133 @@ class _SignPageState extends State<SignPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Form(
-            key: _formKey,
-            autovalidateMode: _autovalidateMode,
-            child: ListView(
-              shrinkWrap: true,
-              children: [
-                Image.asset(
-                  'assets/images/flutter_logo.png',
-                  width: 250,
-                  height: 250,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  autocorrect: false,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    filled: true,
-                    labelText: 'Email',
-                    prefixIcon: Icon(Icons.email),
-                  ),
-                  validator: (String? value) {
-                    if (value == null || value.trim().isEmpty) {
-                      return 'Email required';
-                    }
-                    if (!isEmail(value.trim())) {
-                      return 'Enter a valid email';
-                    }
-                    return null;
-                  },
-                  onSaved: (String? value) {
-                    _email = value;
-                  },
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    filled: true,
-                    labelText: 'Password',
-                    prefixIcon: Icon(Icons.lock),
-                  ),
-                  validator: (String? value) {
-                    if (value == null || value.trim().isEmpty) {
-                      return 'Password required';
-                    }
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: BlocConsumer<SigninCubit, SigninState>(
+          listener: (context, state) {
+            // TODO: implement listener
+            if (state.signinStatus == SigninStatus.error) {
+              // made a custom dialog field for eeror
+              errorDialog(context, state.error);
+            }
+          },
+          builder: (context, state) {
+            return Scaffold(
+              body: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: Form(
+                    key: _formKey,
+                    autovalidateMode: _autovalidateMode,
+                    child: ListView(
+                      shrinkWrap: true,
+                      children: [
+                        Image.asset(
+                          'assets/images/flutter_logo.png',
+                          width: 250,
+                          height: 250,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          autocorrect: false,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            labelText: 'Email',
+                            prefixIcon: Icon(Icons.email),
+                          ),
+                          validator: (String? value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return 'Email required';
+                            }
+                            if (!isEmail(value.trim())) {
+                              return 'Enter a valid email';
+                            }
+                            return null;
+                          },
+                          onSaved: (String? value) {
+                            _email = value;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        TextFormField(
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            labelText: 'Password',
+                            prefixIcon: Icon(Icons.lock),
+                          ),
+                          validator: (String? value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return 'Password required';
+                            }
 
-                    if (value.trim().length < 6) {
-                      return 'Password must be at least 6 characters long';
-                    }
-                    return null;
-                  },
-                  onSaved: (String? value) {
-                    _password = value;
-                  },
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                ElevatedButton(
-                  onPressed: _submit,
-                  style: ElevatedButton.styleFrom(
-                    textStyle: const TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
+                            if (value.trim().length < 6) {
+                              return 'Password must be at least 6 characters long';
+                            }
+                            return null;
+                          },
+                          onSaved: (String? value) {
+                            _password = value;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+
+                        // if the signin button is on submitting state then it will execute the function or it will return null
+                        ElevatedButton(
+                          onPressed:
+                              state.signinStatus == SigninStatus.submitting
+                                  ? null
+                                  : _submit,
+                          style: ElevatedButton.styleFrom(
+                            textStyle: const TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          ),
+                          // if the state is submitting then it will show loading or it will show signin
+                          child: Text(
+                            state.signinStatus == SigninStatus.submitting
+                                ? 'loading'
+                                : 'Sign In',
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            // if the state is in the submitting state then the button will be disable
+                            state.signinStatus == SigninStatus.submitting
+                                ? null
+                                : Navigator.pushNamed(
+                                    context, SignUpPage.routeName);
+                          },
+                          style: TextButton.styleFrom(
+                              textStyle: const TextStyle(
+                            fontSize: 20.0,
+                            decoration: TextDecoration.underline,
+                          )),
+                          child: const Text('Not a Member? Sign Up!'),
+                        )
+                      ],
                     ),
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                  ),
-                  child: const Text(
-                    'Sign In',
                   ),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, SignUpPage.routeName);
-                  },
-                  style: TextButton.styleFrom(
-                      textStyle: const TextStyle(
-                    fontSize: 20.0,
-                    decoration: TextDecoration.underline,
-                  )),
-                  child: const Text('Not a Member? Sign Up!'),
-                )
-              ],
-            ),
-          ),
+              ),
+            );
+          },
         ),
       ),
     );
